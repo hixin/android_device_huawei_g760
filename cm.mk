a48d919fc37b5b04ee9f3f593a70ad5df219159e
mparus <mikael.parus@gmail.com>
2016-01-16 11:58:15 +0100

    initial

---
+++ cm.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/huawei/g760/full_g760.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_g760
+PRODUCT_DEVICE := g760
+
+PRODUCT_GMS_CLIENTID_BASE := android-G760
+
+# Unofficial build ID
+#TARGET_UNOFFICIAL_BUILD_ID := Desalesouche
+TARGET_UNOFFICIAL_BUILD_ID := mparus
